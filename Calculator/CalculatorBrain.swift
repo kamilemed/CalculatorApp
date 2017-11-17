8f94c505912c8c54f41afbb4c87d9b61f91ffe50
Kamilė Medzevičiūtė <medzeviciute.kamile@gmail.com>
2017-11-17 10:54:19 +0200

    initial commit

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+
+private func makeFlowtingNumber(op1: Double, op2: Double) -> Double {
+    var fraction = op2
+    while fraction > 1 {
+        fraction /= 10
+    }
+    return op1 + fraction
+}
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "𝛑": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "±" : Operation.unaryOperation({ -$0 }),
+        "+" : Operation.binaryOperation({ $0 + $1 }),
+        "-" : Operation.binaryOperation({ $0 - $1 }),
+        "×" : Operation.binaryOperation({ $0 * $1 }),
+        "÷" : Operation.binaryOperation({ $0 / $1 }),
+        "." : Operation.binaryOperation(makeFlowtingNumber),
+        "=" : Operation.equals
+    ]
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            }
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation?.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        get {
+           return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymobl = sender.currentTitle {
+            brain.performOperation(mathematicalSymobl)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
 }
 
